5dbe727c7508962cb5cc93a28d468dbb84ba608e
Debarshee Chakraborty <debarsheechakraborty.11d@gmail.com>
2025-06-24 11:15:59 +0530

    feat: Added chrome extension configiuration for the extenstion client

--- extension-client/vite.config.ts
+++ extension-client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { crx } from "@crxjs/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import manifest from "./manifest.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss(), crx({ manifest })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
